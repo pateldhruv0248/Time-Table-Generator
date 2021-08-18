ebbaaed4332f716fe01a5e1a479aa5344b75495b
Dhruv <pateldhruv0248@gmail.com>
2021-08-19 01:24:55 +0530

    Days and Slots variable added

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from pprint import pprint
 
-classes = {'C1', 'C2', 'C3'}
+classes = {'C1', 'C2', 'C3', 'C4', 'C5', 'C6'}
+weekdays = [' Mon  ', ' Tue  ', ' Wed  ', ' Thu  ', ' Fri  ']
+Days = 5
+Slots = 6
 
 class Faculty:
     def __init__(self, facultyName, numberOfClass) -> None:
         self.facultyName = facultyName
         self.numberOfClass = numberOfClass
-        self.timeTable = [[[cls for cls in classes] for i in range(6)] for j in range(5)]
+        self.timeTable = [[[cls for cls in classes] for i in range(Days)] for j in range(Slots)]
 
 def showFreeSlots(faculty):
-    for day in range(5):
-        for slot in range(6):
-            if len(faculty.timeTable[day][slot]) > 1:
-                faculty.timeTable[day][slot] = ['--']
+    for slot in range(Slots):
+        for day in range(Days):
+            if len(faculty.timeTable[slot][day]) > 1:
+                faculty.timeTable[slot][day] = ['--']
 
 def removeClassFromOtherFaculties(asssignedFaculty, classroom, day, slot):
     for faculty in allFaculties:
@@ -23,28 +26,33 @@ def removeClassFromOtherFaculties(asssignedFaculty, classroom, day, slot):
                 pass
 
 def getFacultyTimeTable(faculty):
-    for day in range(5):
-        for slot in range(6):
-            currentSLotArray = faculty.timeTable[day][slot]
+    for slot in range(Slots):
+        for day in range(Days):
+            currentSLotArray = faculty.timeTable[slot][day]
             for classroom in range(len(currentSLotArray)):
-                assignClass = faculty.timeTable[day][slot][classroom]
+                assignClass = faculty.timeTable[slot][day][classroom]
                 if faculty.numberOfClass[assignClass] > 0:
-                    faculty.timeTable[day][slot] = [assignClass]
+                    faculty.timeTable[slot][day] = [assignClass]
                     faculty.numberOfClass[assignClass] = faculty.numberOfClass[assignClass] - 1
-                    removeClassFromOtherFaculties(faculty, assignClass, day, slot)
+                    removeClassFromOtherFaculties(faculty, assignClass, slot, day)
                     break
     showFreeSlots(faculty)
 
 def printTimeTable(faculty):
     print('Faculty Name :- ', faculty.facultyName)
-    for day in range(5):
-        pprint(faculty.timeTable[day])
+    print(*weekdays)
+    for slot in range(Slots):
+        print(*faculty.timeTable[slot])
     print()
-F1 = Faculty('F1', {'C1': 3, 'C2': 4,  'C3': 4})
-F2 = Faculty('F2', {'C1': 5, 'C2': 7,  'C3': 3})
 
-allFaculties = [F1, F2]
-getFacultyTimeTable(F2)
-getFacultyTimeTable(F1)
-printTimeTable(F2)
-printTimeTable(F1)
+#input of every faculty member, showing total number of lectures to be taught every week.
+F1 = Faculty('Faculty 1', {'C1': 3, 'C2': 4,  'C3': 4, 'C4':3, 'C5':2, 'C6':4})
+F2 = Faculty('Faculty 2', {'C1': 5, 'C2': 7,  'C3': 6, 'C4':2, 'C5':3, 'C6':3})
+F3 = Faculty('Faculty 3', {'C1': 2, 'C2': 7,  'C3': 5, 'C4':5, 'C5':0, 'C6':1})
+F4 = Faculty('Faculty 4', {'C1': 2, 'C2': 2,  'C3': 5, 'C4':7, 'C5':6, 'C6':4})
+
+allFaculties = [F1, F2, F3, F4]
+
+for faculty in allFaculties:
+    getFacultyTimeTable(faculty)
+    printTimeTable(faculty)
